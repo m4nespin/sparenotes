c5d78ce6d87c6ef9b052b62c827aa12ef1b2f5f4
Emmanuel Espinal <m4nucodes@pm.me>
2026-08-16 20:35:29 -0400

    release: 0.2.0-beta

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "app.sparenotes"
         minSdk = 30
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.1.1"
+        versionCode = 3
+        versionName = "0.2.0-beta"
     }
 
     compileOptions {
